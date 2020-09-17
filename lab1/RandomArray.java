18ecd2de430b58ba37624ae0776cd2e094d002bf
Maxim Glazkov <53126226+AllueXD@users.noreply.github.com>
2020-09-17 14:00:38 +0300

    kavo
    1//

---
+++ lab1/RandomArray.java
@@ -0,0 +1,28 @@
+package lab1;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class RandomArray {
+    private int[] array = new int[30];
+    public void fillArrayWithRandom(){
+        Random random = new Random();
+        for (int i = 0; i < this.array.length; i++){
+            this.array[i] = random.nextInt(100);
+        }
+    }
+    public void fillArrayWithMath(){
+        for (int i = 0; i < this.array.length; i++){
+            this.array[i] = (int)(Math.random() * 100);
+        }
+    }
+    public void sortArray(){
+        Arrays.sort(this.array);
+    }
+    public void showArray(){
+        for (int i = 0; i < this.array.length; i++){
+            System.out.print(this.array[i] + " ");
+        }
+        System.out.println();
+    }
+}
